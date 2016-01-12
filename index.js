0b31e06d275c3e19d578cc454a9941e53838f120
John Nguyen <nguy0052@gmail.com>
2016-01-12 11:26:34 -0500

    Add source

---
+++ index.js
@@ -0,0 +1,101 @@
+var ttl = require('level-ttl');
+var levelDefaults = require('levelup-defaults');
+var xtend = require('xtend');
+var util = require('util');
+var debug = require('debug')('express-session-level');
+
+module.exports = function(session) {
+	var Store = session.Store;
+	var noop = function() {};
+
+	function LevelSession(db, options) {
+		this.options = options = xtend(options);
+		db = levelDefaults(db, {
+			valueEncoding: 'json'
+		});
+
+		options.levelTTLOptions = 'levelTTLOptions' in options ?
+			options.levelTTLOptions:
+			{
+				defaultTTL:     1000*60*15, // 15 minutes
+				checkFrequency: 1000*60*30  // 30 minutes
+			};
+
+		options.prefix = 'prefix' in options ? options.prefix : '';
+
+		this.db = ttl(db, this.options.levelTTLOptions);
+	}
+
+	util.inherits(LevelSession, Store);
+
+	LevelSession.prototype.get = function(sid, cb) {
+		sid = this.getKey(sid);
+		cb = cb || noop;
+
+		debug('GET %s', sid);
+		this.db.get(sid, function(err, data) {
+			if (err && err.notFound) {   // no found
+				debug('GET %s Notfound', sid);
+				return cb(null, null);
+			}
+			else if (err) {  // propagate db error
+				debug('GET %s Error %s', sid, err);
+				return cb(err);
+			}
+			else {
+				debug('GET %s Success', sid);
+				return cb(null, data);
+			}
+		});
+	}
+
+	LevelSession.prototype.set = LevelSession.prototype.touch = function(sid, session, cb) {
+		sid = this.getKey(sid);
+		cb = cb || noop;
+
+		debug('SET %s, %o', sid, session);
+		this.db.put(sid, session, function(err) {
+			if (err) {
+				debug('SET %s ERROR %s', sid, err);
+				return cb(err);
+			}
+
+			debug('SET %s SUCCESS', sid);
+			cb(null);
+		});
+	}
+
+	LevelSession.prototype.destroy = function(sid, cb) {
+		debug('DESTROY %s', sid);
+		sid = this.getKey(sid);
+		cb = cb || noop;
+
+		debug('DESTROY %s', sid);
+		this.db.del(sid, function(err) {
+			if (err) {
+				debug('DESTROY %s ERROR %s', sid, err);
+				return cb(err);
+			}
+
+			debug('DESTROY %s SUCCESS', sid);
+			cb(null);
+		});
+	}
+
+	LevelSession.prototype.length = function(sid, cb) {
+		var count = 0;
+		cb = cb || noop;
+
+		this.db.createReadStream({keys: true, values: false})
+			.on('data', function() { count++ })
+			.on('end', function() { cb(null, count) })
+			.on('error', function(err) { cb(err) })
+		;
+	}
+
+	LevelSession.prototype.getKey = function(sid) {
+		return this.options.prefix + sid;
+	}
+
+	return LevelSession;
+}
---
+++ test.js
@@ -0,0 +1,73 @@
+var os = require('os');
+var path = require('path');
+var level = require('level');
+var session = require('express-session');
+var levelSession = require('./');
+var test = require('tape');
+
+var tmpDir = path.join(os.tmpDir(), 'level-session-' + Math.random());
+var Store = levelSession(session);
+
+test('life cycle', function(t) {
+	var leveldb = level(tmpDir);
+	var store = new Store(leveldb);
+	var obj = { cookie: { maxAge: 2000 }, name: 'tj' };
+
+	store.set('123', obj, assertSet);
+
+	function assertSet(err) {
+		t.notOk(err, 'should not have error on SET');
+		store.get('123', assertGet);  // continue Get test
+	}
+
+	function assertGet(err, val) {
+		t.notOk(err, 'should not have error on GET');
+		t.deepEqual(val, obj, 'stored value should be correct');
+		store.destroy('123', assertDestroy);  // continue destroy test
+	}
+
+	function assertDestroy(err) {
+		t.notOk(err, 'should not have error on DESTROY');
+
+		store.get('123', function(err, val) {  // asert '123' is NotFound
+			t.notOk(err);
+			t.equal(val, null, 'sessionId should not be found anymore');
+
+			store.db.close(t.end);  // close leveldb and end test
+		});
+	}
+});
+
+test('options: prefix', function(t) {
+	var leveldb = level(tmpDir);
+	var store = new Store(leveldb, {prefix: 'prefix!'});
+	var obj = { cookie: { maxAge: 2000 }, name: 'tj' };
+
+	store.set('123', obj, assertSet);
+
+	function assertSet(err) {
+		t.notOk(err);
+		store.get('123', assertGet);  // continue Get test
+	}
+
+	function assertGet(err, val) {
+		t.notOk(err);
+		t.deepEqual(val, obj, 'data should be retrievable');
+
+		leveldb.get('prefix!123', function(err, val) {
+			t.notOk(err);
+			t.deepEqual(JSON.parse(val), obj, 'data should be stored with prefix set');
+
+			store.db.close(t.end);  // close leveldb and end test
+		});
+	}
+});
+
+test('options:levelTTLOptions', function(t) {
+	var leveldb = level(tmpDir);
+	var store = new Store(leveldb, {levelTTLOptions: {checkFrequency: 15000}});
+
+	t.equal(store.db._ttl.options.checkFrequency, 15000,
+		'checkFrequency options should be set to levelTTL instance');
+	store.db.close(t.end);  // close leveldb and end test
+});
